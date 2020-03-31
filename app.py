0898bcef4bcced55991d7d1893cc3cbb6093124a
Sayonsom Chanda <sayon@ieee.org>
2020-03-31 05:59:30 +0530

    Added percent change charts

--- app.py
+++ app.py
@@ -3,10 +3,11 @@ import plotly.express as px
 import json
 from scipy.integrate import odeint
 from scipy.optimize import linprog
+import plotly.figure_factory as ff
 import re
 import requests
 from datetime import datetime, timedelta
-import altair as alt
+from bokeh.plotting import figure
 import pandas as pd
 import numpy as np
 
@@ -21,6 +22,16 @@ def one_diff(val_1, val_2):
     except:
         x = 0
     return x
+
+
+def seven_day_average(values):
+    """
+    Return a list of seven day averages
+    """
+    r_values = values[-7:]
+    rate = [one_diff(r_values[1], r_values[0]), one_diff(r_values[2], r_values[1]), one_diff(r_values[3], r_values[2]), one_diff(r_values[4], r_values[3]), one_diff(r_values[5], r_values[4]), one_diff(r_values[6], r_values[5])]
+    rate = [r*100 for r in rate]
+    return rate
 
 
 def rate(values):
@@ -63,7 +74,8 @@ def get_corona_data(country_name):
     case_rate = rate(confirmed)
     death_rate = rate(deaths)
     recovery_rate = rate(recovery)
-    return dates, deaths, confirmed, recovery, case_rate, death_rate, recovery_rate, date_of_spread_start
+    seven_day_cases_pc, seven_day_deaths_pc = seven_day_average(deaths), seven_day_average(confirmed)
+    return dates, deaths, confirmed, recovery, case_rate, death_rate, recovery_rate, date_of_spread_start, seven_day_cases_pc, seven_day_deaths_pc
 
 def sir_model(total_population,infected,recovered, contact_rate, recovery_rate, death_rate):
     S0 = total_population - infected - recovered
@@ -92,7 +104,7 @@ population = pop_data[user_country]['population']
 country_rank = pop_data[user_country]['rank']
 population_density = pop_data[user_country]['pop_density']
 st.sidebar.markdown("##### The community outside")
-lockdown_status = st.sidebar.radio("How strictly is lockdown/shelter-in-place being enforced?", ["No one's allowed out", "See only a few people out", "People are secretly flouting it", "People are openly flouting it"])
+lockdown_status = st.sidebar.radio("How strictly is lockdown/shelter-in-place being enforced?", ["No one's allowed out", "See only a few people out", "People are secretly not following rules", "People are openly not following rules"])
 
 # st.sidebar.markdown("##### The Human Network and Health System Response")
 # st.sidebar.markdown("> Play with this section to see how governmental initiatives, such as contact tracing, can affect the rate of infected and thus, the death rate")
@@ -164,12 +176,12 @@ if lockdown_status == "No one's allowed out":
     lockdown_intensity_value = 0.1
 elif lockdown_status == "See only a few people out":
     lockdown_intensity_value = 0.33
-elif lockdown_status == "People are secretly flouting it":
+elif lockdown_status == "People are secretly not following rules":
     lockdown_intensity_value = 0.66
-elif lockdown_status == "People are openly flouting it":
+elif lockdown_status == "People are openly not following rules":
     lockdown_intensity_value = 1
 
-dates, deaths, confirmed, recovery, case_rate, death_rate, recovery_rate, date_of_spread_start = get_corona_data(user_country)
+dates, deaths, confirmed, recovery, case_rate, death_rate, recovery_rate, date_of_spread_start, seven_day_cases_pc, seven_day_deaths_pc = get_corona_data(user_country)
 
 try:
     death_to_case_ratio = deaths[-1] / confirmed[-1]
@@ -248,7 +260,7 @@ def optimization_model(country_rank, population, susceptible, infected, lockdown
             health_care_capacity_percent = 0.05/100
             max_sick_people = health_care_capacity_percent * population
 
-    print(total_susceptibles, total_infections)
+    # print(total_susceptibles, total_infections)
  
     c = [lockdown_intensity_value*REPRODUCTIVE_RATE, -1*network_degree*REPRODUCTIVE_RATE]
     A = [[-1*network_degree*REPRODUCTIVE_RATE, 2*REPRODUCTIVE_RATE],[ (0.1)*REPRODUCTIVE_RATE, -1*network_degree*REPRODUCTIVE_RATE]]
@@ -301,6 +313,9 @@ else:
 
 # st.markdown(f"Today is day {len(dates)} of COVID-19.")
 
+
+
+
 chart_data = pd.DataFrame({
     'Deaths': deaths,
     'Confirmed': confirmed}
@@ -308,12 +323,48 @@ chart_data = pd.DataFrame({
 
 st.line_chart(chart_data)
 
+
+
+
+
+
 if case_rate > 1.25:
     st.markdown(f"> Our model suggests that, __in {int(days_left/7)} weeks, that is, by {good_news_date}__, everything will be safe and normal again. There is a risk of {actual_infections:,} cases in {user_country}. Most of these cases will be minor. However, the number of __lives lost__ _can_ be as high as __{int(actual_infections*death_to_case_ratio):,}__.")
 else:
     st.markdown(f"> Our model suggests that, the pandemic is not a major crisis in {user_country} at this moment. Still there is a risk of {actual_infections:,} cases in {user_country}. Most of these cases will be minor. It is still important to note that, the number of __lives lost__ _can_ be as high as __{int(actual_infections*death_to_case_ratio):,}__, as well as chance of a new outbreak.")
 
 st.markdown(f"Of these {actual_infections:,} cases, about {int(0.2*actual_infections):,} will require some degree of hospitilization. The health care system in {user_country} is prepared for approximately {int(max_sick_people):,} people.")
+
+st.markdown("## What was the daily percent change in cases and deaths, over the last week?")
+
+days = [today - timedelta(days=5), today - timedelta(days=4), today - timedelta(days=3), today - timedelta(days=2), today - timedelta(days=1), today]
+days = [d.strftime("%b %d") for d in days]
+chart_data = pd.DataFrame({
+    "Days": days,
+    'Cases %': seven_day_cases_pc,
+    'Deaths %': seven_day_deaths_pc}
+)
+chart_data = chart_data.rename(columns={'Days':'index'}).set_index('index')
+st.line_chart(chart_data)
+
+
+# p = figure(title='Case % / Death %',
+#           x_axis_label="Cases %",
+#           y_axis_label="Deaths %")
+
+# p.line(days, seven_day_cases_pc)
+# p.line(days, seven_day_deaths_pc)
+# st.bokeh_chart(p, use_container_width=True)
+
+
+chart_data = pd.DataFrame({
+    'People at Risk': S,
+    'Confirmed Cases': I,
+    'Removed from Risk': R}
+)
+
+st.line_chart(chart_data)
+
 
 st.write("""
  This model:
@@ -326,13 +377,6 @@ st.write("""
 Please feel free to play around with other parameters to see the impact of the virus.
 """)
 
-chart_data = pd.DataFrame({
-    'People at Risk': S,
-    'Confirmed Cases': I,
-    'Removed from Risk': R}
-)
-
-st.line_chart(chart_data)
 
 st.markdown(f"## What will be the number of cases on ... ?")
 
@@ -381,7 +425,7 @@ st.markdown(f"In the _last five days_, the death rate has been {death_rate:.2f} 
 
 
 st.markdown(f"- As a whole nation, people of {user_country} can be attributed a__ worry index score of {worry_index:.3f}__, where values close to 0 are good, and _if_ it is close to 1 - you should take more precautions.")
-st.markdown(f"- In the _next five days_, your chance of getting an infection is __{probability_of_getting_infected*100:.2f}%__. This is because people are {lockdown_status}.")
+st.markdown(f"- In the _next five days_, your chance of getting an infection is __{probability_of_getting_infected*100:.2f}%__. This is because  {lockdown_status}.")
 st.markdown(f"- In the total period of crisis, the peak probability of getting infected will come in about {int(I_max_index/7)} weeks. During the __week of {peak_date}__, the chance that a previously un-affected person will get COVID-19 is expected to be  __{probability_of_getting_infected_max*100:.2f}%__. ")
 st.write("""
 This is how we calculated the worry index: 
@@ -392,14 +436,13 @@ where $w_L$ is a weight associated with the severity of the lockdown intensity, 
 """)
 
 st.markdown(f"#### Concern for self, family, and friends")
-print(hygiene_status)
+
 
 if hygiene_status == "Yes":
     personal_worry_index = 0.3 * (age_group_risk_weight / 4 + preexisting_conditions_weight / 2)
 else:
     personal_worry_index = 0.75 * (age_group_risk_weight / 4 + preexisting_conditions_weight / 2)
 
-print()
 
 personal_worry_index_total = 1 / 10 * (worry_index + 4*personal_worry_index + 2*(1/8)*current_health_index + 4*(1/4)*travel_history_index)
 if personal_worry_index_total < 0.33:
